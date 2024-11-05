0fd47fe67a712ffdfbb5457aab9928b767359538
stevenpiao0822 <alenschmitt415@gmail.com>
2024-11-04 23:25:51 -0800

    [create] created miningToken contract

---
+++ contracts/MiningToken.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.27;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract MiningToken is ERC20Capped, ERC20Burnable {
+
+}
