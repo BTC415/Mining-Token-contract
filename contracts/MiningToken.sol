1ddcd930106b57feff1b261521bbc008a35b991a
stevenpiao0822 <alenschmitt415@gmail.com>
2024-11-04 23:38:13 -0800

    [add] added constructor function

--- contracts/MiningToken.sol
+++ contracts/MiningToken.sol
@@ -6,5 +6,15 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract MiningToken is ERC20Capped, ERC20Burnable {
+    address payable public owner;
+    uint256 public blockReward;
 
+    constructor(
+        uint256 cap,
+        uint256 reward
+    ) ERC20("MintingToken", "MNT") ERC20Capped(cap * 10 ** decimals()) {
+        owner = payable(msg.sender);
+        _mint(owner, 5000000 * 10 ** decimals());
+        blockReward = reward * 10 ** decimals();
+    }
 }
